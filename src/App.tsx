96b718ea79228a0a21d06af0218eeb9be88442ec
Abd El Hakim Akhadkhou <akprogamer23@gmail.com>
2023-10-16 10:46:39 +0300

    beta done

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,11 @@ type Point = { x: number; y: number };
 
 function App() {
 
-  const cursor = useRef(null);
+  const sizes = [5,12,16,24,32,40,45]
+
+  const colors = ["#202020","#6A9C89", "#C1D8C3", "#F5E8B7", "#CD5C08"];
+
+  const [cursorPos, setCursorPos] = useState({x:"0px",y:"0px"});
 
   const [size, setSize] = useState(5)
 
@@ -33,30 +37,31 @@ function App() {
 
     ctx.fillStyle = color;
     ctx.beginPath();
-    ctx.arc(startPoint.x, startPoint.y, 2, 0, 2 * Math.PI);
+    ctx.arc(startPoint.x, startPoint.y, size/2, 0, size/2 * Math.PI);
     ctx.fill();
   }
 
   const handleMouse = (e: MouseEvent) => {
-    const cur = cursor.current! as HTMLDivElement;
-
-    cur.style.left = e.clientX - size/2 + "px";
-    cur.style.top = e.clientY - size/2 + "px";
+    setCursorPos({
+      x: e.clientX - size + "px",
+      y: e.clientY - size + "px"
+    });
   };
 
   useEffect(() => {
     window.addEventListener('mousemove', handleMouse);
     return () => window.removeEventListener('mousemove', handleMouse);
-  }, [cursor])
+  }, [cursorPos])
 
   return (
-    <div className="h-screen grid place-content-center">
+    <div className="h-screen grid place-items-center">
       <div
         style={{
           width: size * 2 + "px",
           height: size * 2 + "px",
+          left: cursorPos.x,
+          top: cursorPos.y
         }}
-        ref={cursor}
         className="absolute border-2 rounded-full select-none pointer-events-none border-black"
       ></div>
       <canvas
@@ -67,22 +72,36 @@ function App() {
         className="border-2 border-black"
       />
       <div id="buttons" className="flex w-[700px] m-auto justify-center">
-        <div
-          onClick={() => setColor("#000")}
-          className="w-24 h-24 bg-[#000]"
-        ></div>
-        <div
+        {colors.map(color => 
+          <button
+            onClick={() => setColor(color)}
+            style={{ backgroundColor: color }}
+            className="w-24 h-24"
+          ></button>
+        )}
+        <button
           onClick={() => setColor("#fff")}
           className="w-24 h-24 border-2 border-black grid place-content-center text-xl select-none"
         >
           ERASER
-        </div>
-        <div
+        </button>
+        <button
           onClick={clear}
           className="w-24 h-24 border-2 border-black grid place-content-center text-xl select-none"
         >
           CLEAR
-        </div>
+        </button>
+      </div>
+      <div className="flex mt-4 gap-8">
+        {sizes.map(size => (
+          <button onClick={() => setSize(size)} className="flex items-center">
+            <label className="mx-4">{size}</label>
+            <span style={{
+              width: size*2+"px",
+              height: size*2+"px"
+            }} className="rounded-full border-2 border-black"></span>
+          </button>
+        ))}
       </div>
     </div>
   );
