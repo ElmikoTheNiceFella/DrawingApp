a946bcfaa0f239745356e8e1c6f6ca9a12794f84
Abd El Hakim Akhadkhou <akprogamer23@gmail.com>
2023-10-27 14:32:17 +0300

    Done

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,14 @@ function App() {
 
   const sizes = [5,12,16,24,32,40,45]
 
-  const colors = ["#202020","#6A9C89", "#C1D8C3", "#F5E8B7", "#CD5C08"];
+  const colors = [
+    "#202020",
+    "#6A9C89",
+    "#C1D8C3",
+    "#F5E8B7",
+    "#CD5C08",
+    "#ADD8E6",
+  ];
 
   const [cursorPos, setCursorPos] = useState({x:"0px",y:"0px"});
 
@@ -54,15 +61,16 @@ function App() {
   }, [cursorPos])
 
   return (
-    <div className="h-screen grid place-items-center">
+    <div className="h-screen grid place-items-center overflow-hidden relative">
       <div
         style={{
           width: size * 2 + "px",
           height: size * 2 + "px",
           left: cursorPos.x,
-          top: cursorPos.y
+          top: cursorPos.y,
+          backgroundColor: color,
         }}
-        className="absolute border-2 rounded-full select-none pointer-events-none border-black"
+        className="absolute border-2 rounded-full z-50 select-none pointer-events-none border-black"
       ></div>
       <canvas
         onMouseDown={onMouseDown}
@@ -71,36 +79,73 @@ function App() {
         height={500}
         className="border-2 border-black"
       />
-      <div id="buttons" className="flex w-[700px] m-auto justify-center">
-        {colors.map(color => 
+      <div
+        id="buttons"
+        style={{
+          gridTemplateColumns: "6rem 6rem",
+        }}
+        className="grid absolute mt-[-19.3rem] ml-[1400px] w-[700px]"
+      >
+        {colors.map((color) => (
           <button
             key={color}
             onClick={() => setColor(color)}
             style={{ backgroundColor: color }}
             className="w-24 h-24"
           ></button>
-        )}
+        ))}
         <button
           onClick={() => setColor("#fff")}
-          className="w-24 h-24 border-2 border-black grid place-content-center text-xl select-none"
+          className="w-24 h-24 border-y-2 border-l-2 border-r-[1px] border-black grid place-content-center text-xl select-none"
         >
           ERASER
         </button>
         <button
           onClick={clear}
-          className="w-24 h-24 border-2 border-black grid place-content-center text-xl select-none"
+          className="w-24 h-24 border-y-2 border-l-[1px] border-r-2 border-black grid place-content-center text-xl select-none"
         >
           CLEAR
         </button>
       </div>
-      <div className="flex mt-4 gap-8">
-        {sizes.map(size => (
-          <button key={"size"+size} onClick={() => setSize(size)} className="flex items-center">
-            <strong className="mx-4">{size}</strong>
-            <span style={{
-              width: size*2+"px",
-              height: size*2+"px"
-            }} className="rounded-full border-2 border-black"></span>
+      <div className="flex items-center -mt-56 gap-8">
+        {sizes.map((size) => (
+          <button
+            style={{
+              backgroundColor: color,
+            }}
+            key={"size" + size}
+            onClick={() => setSize(size)}
+            className="flex items-center border-2 border-black h-fit pr-4 py-2 rounded-xl"
+          >
+            {/*"#C1D8C3", "#F5E8B7"*/}
+            <strong
+              style={{
+                color:
+                  color == "#C1D8C3" ||
+                  color == "#F5E8B7" ||
+                  color == "#fff" ||
+                  color == "#ADD8E6"
+                    ? "black"
+                    : "white",
+              }}
+              className="mx-4"
+            >
+              {size}
+            </strong>
+            <span
+              style={{
+                width: size * 2 + "px",
+                height: size * 2 + "px",
+                borderColor:
+                  color == "#C1D8C3" ||
+                  color == "#F5E8B7" ||
+                  color == "#fff" ||
+                  color == "#ADD8E6"
+                    ? "black"
+                    : "white",
+              }}
+              className="rounded-full border-2"
+            ></span>
           </button>
         ))}
       </div>
